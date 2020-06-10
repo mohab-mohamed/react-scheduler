71fea3088b00acb7fd8fa146a1745add7b88e65e
Mohab <mohab8m@gmail.com>
2020-06-10 18:30:14 -0400

    initial commit

--- server.js
+++ server.js
@@ -1,12 +1,20 @@
 const express = require("express");
+const passport = require("passport");
+const mongoose = require("mongoose");
+const keys = require("./config/keys");
+const cookieSession = require("cookie-session");
 const path = require("path");
 const PORT = process.env.PORT || 3001;
 const app = express();
+
+require("./models/User");
+require("./services/passport");
 
 // Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 }
+
 
 // Send every request to the React app
 // Define any API routes before this runs
